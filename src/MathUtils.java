16b42bed77c2a30f5e9d513a9113b6a3246d1a53
bheffron <bheffron@purdue.edu>
2018-10-02 14:31:26 -0400

    Debugged MathUtils.java and StringUtils.java

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,7 +3,7 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    public static double divideNumbers(int dividend, double divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
         double res = dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
@@ -16,26 +16,31 @@ public class MathUtils {
      */
     public static double raiseToPower(int base, int exp) {
         /* Set initial result to be just the number */
-        int res = base;
+        double res = base;
 
         /* Determine if the exponent is negative */
         boolean isNegative = false;
         if (exp < 0) {
-            exp *= -1;
             isNegative = true;
         }
 
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
-        for (int i = 0; i < exp; i++) {
-           res *= base; 
+        if(isNegative) {
+            exp *= -1;
+            for (int i = 1; i < exp; i++) {
+                res = Math.pow(base, exp);
+            }
         }
-
+        else {
+            for(int i = 1; i < exp; i++){
+                res = Math.pow(base, exp);
+            }
+        }
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
-            return dividedRes;
+            return divideNumbers(1,res);
         }
         /* Otherwise, we are safe to just return the result */
         else {
